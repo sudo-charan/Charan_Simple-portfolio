4de91e28c84801fae3423a2eea8083686e297205
sudo-charan <charanaikofficial@gmail.com>
2024-03-26 22:36:58 +0530

    first commit

---
+++ js/script.js
@@ -0,0 +1,97 @@
+// Typed-Text---------
+var typed = new Typed('#typetext', {
+  strings: ["Fullstak Developer", "Web Developer", "Coding Enthusiast"],
+  typeSpeed: 100,
+  backSpeed: 100,
+  backDelay: 1000,
+  loop: true
+});
+// Typed-Text End-----
+
+document.addEventListener("DOMContentLoaded", function () {
+  const navLinks = document.querySelectorAll(".nav-box ul li a");
+
+  navLinks.forEach((link) => {
+    link.addEventListener("click", function (event) {
+      navLinks.forEach((link) => {
+        link.classList.remove("active");
+      });
+      this.classList.add("active");
+    });
+  });
+
+  highlightActiveNavLink();
+});
+
+function highlightActiveNavLink() {
+  const sections = document.querySelectorAll(".section");
+
+  window.addEventListener("scroll", () => {
+    let fromTop = window.scrollY + 100;
+
+    sections.forEach((section) => {
+      if (section.offsetTop <= fromTop && section.offsetTop + section.offsetHeight > fromTop) {
+        document.querySelectorAll(".nav-box ul li a").forEach((link) => {
+          link.classList.remove("active");
+        });
+        const correspondingLink = document.querySelector(`.nav-box ul li a[href="#${section.id}"]`);
+        if (correspondingLink) {
+          correspondingLink.classList.add("active");
+        }
+      }
+    });
+  });
+}
+
+// Animated icons for social media icons-----------------
+document.addEventListener('DOMContentLoaded', function () {
+  const iconContainers = document.querySelectorAll('.icon-container');
+
+  iconContainers.forEach(container => {
+    const animationPath = container.getAttribute('data-animation-path');
+    const animation = lottie.loadAnimation({
+      container: container,
+      renderer: 'svg',
+      loop: true, // set loop to false by default
+      autoplay: false,
+      path: animationPath
+    });
+
+    container.addEventListener('mouseenter', function () {
+      animation.play();
+    });
+
+    container.addEventListener('mouseleave', function () {
+      if (container.dataset.loop !== 'false') {
+        animation.stop();
+      }
+    });
+  });
+});
+
+// To avoide loop animation of instagram icon
+document.addEventListener('DOMContentLoaded', function () {
+  const iconContainers = document.querySelectorAll('.icon-container1');
+
+  iconContainers.forEach(container => {
+    const animationPath = container.getAttribute('data-animation-path');
+    const animation = lottie.loadAnimation({
+      container: container,
+      renderer: 'svg',
+      loop: false,
+      autoplay: false,
+      path: animationPath
+    });
+
+    container.addEventListener('mouseenter', function () {
+      animation.play();
+    });
+
+    container.addEventListener('mouseleave', function () {
+      if (container.dataset.loop !== 'false') {
+        animation.stop();
+      }
+    });
+  });
+});
+// Animated icons for social media icons End---------
